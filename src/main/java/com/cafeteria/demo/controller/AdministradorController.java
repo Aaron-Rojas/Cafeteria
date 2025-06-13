9e0b571677375d898e8239e1f6ea8e22ad8d89f6
chadowcito <creperland12@hotmail.com>
2025-06-13 03:50:46 -0500

    Ya elimina.

--- src/main/java/com/cafeteria/demo/controller/AdministradorController.java
+++ src/main/java/com/cafeteria/demo/controller/AdministradorController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -29,11 +31,19 @@ public String mostrarReservas(Model model) {
     List<Mesa> mesas = mesaService.obtenerTodasLasMesas();
     System.out.println("Total reservas encontradas: " + reservas.size());
     reservas.forEach(System.out::println);
-    model.addAttribute("reservaForm", new Reserva()); // 🔥 evita errores Thymeleaf
+    model.addAttribute("reservaForm", new Reserva()); // evita errores Thymeleaf
     model.addAttribute("listaReservas", reservas);
     model.addAttribute("listaMesas", mesas);
     return "admin";
 }
+
+@PostMapping("/admin/reserva/eliminar")
+public String eliminarReserva(@RequestParam Long id) {
+    reservaService.eliminarReserva(id);
+    return "redirect:/admin"; // REDIRECCIÓN CORRECTA
+}
+
+
 
 @GetMapping("/debug")
 @ResponseBody
